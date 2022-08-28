986f2e9a4b6d22083b648e5858007d9fec9515d8
Emre COŞKUN <emrecoskun06@hotmail.com>
2022-08-28 18:48:06 +0300

    app routing yönlendirmeleri yapıldı. modul bazlı yönlendirmeler ana app rotuing e bağlandı. sırada sayfa tasarımları üzerinden yönetmek var.

--- src/app/admin/components/costomers/costomers.module.ts
+++ src/app/admin/components/costomers/costomers.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CostomersComponent } from './costomers.component';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -9,7 +10,10 @@ import { CostomersComponent } from './costomers.component';
     CostomersComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild([
+       {path: "", component:CostomersComponent}
+    ])
   ]
 })
 export class CostomersModule { }
