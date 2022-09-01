2e45b1fc4b1eb2e59cfd55d6ac0cad2c9670baad
Emre COŞKUN <emrecoskun06@hotmail.com>
2022-09-01 15:19:44 +0300

    prodct üzerinde http metotlarının örnekleri bırakıldı

--- src/app/admin/components/products/products.component.ts
+++ src/app/admin/components/products/products.component.ts
@@ -22,42 +22,25 @@ export class ProductsComponent extends BaseComponent implements OnInit {
       controller:"products"
     }).subscribe(data => console.log(data));
 
-    this.httpClientService.delete({ 
-      controller:"products"
-    },"AAD6F8AD-48BA-4E97-2A09-08DA8AB601B5")
-    .subscribe()
-    //delete metodunu çalıştıramadım. yeniden dene.
+    // this.httpClientService.delete({ 
+    //   controller:"products"
+    // }, "7ED60EC5-ED1F-4C22-BBEE-08DA8C0FF4FC")
+    // .subscribe()
 
     // this.httpClientService.put({
     //   controller:"products"
-    // },{id:"EFEDC33F-6A3D-4FE5-4344-08DA8AB37C2D",
-    //   name:"Değişen Nesne",
-    //   stock:1500,
-    //   price:2.8    
+    // },{id:"F5251E09-A97B-45C4-BBED-08DA8C0FF4FC",
+    //   name:"Yeni nesne güncellendi",
+    //   stock:1,
+    //   price:1.985    
     // }).subscribe()
 
     // this.httpClientService.post({
     //   controller:"products"
     // },{
-    //   name: 'Tavan U ',
+    //   name: 'testtttt ',
     //   stock:5000,
     //   price:3
-    // }).subscribe();
-
-    // this.httpClientService.post({
-    //   controller:"products"
-    // },{
-    //   name: 'Tavan C',
-    //   stock:2500,
-    //   price:5
-    // }).subscribe();
-
-    // this.httpClientService.post({
-    //   controller:"products"
-    // },{
-    //   name: 'Alçı Köşe',
-    //   stock:7000,
-    //   price:10
     // }).subscribe();
 
 
