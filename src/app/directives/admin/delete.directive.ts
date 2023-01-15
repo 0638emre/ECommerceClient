9a0dc6facd3149d3de07d736575019356542925c
Emre COŞKUN <emrecoskun06@hotmail.com>
2023-01-16 00:29:24 +0300

    role yapılanması oluşturuldu.

--- src/app/directives/admin/delete.directive.ts
+++ src/app/directives/admin/delete.directive.ts
@@ -23,7 +23,7 @@ export class DeleteDirective {
     private spinner : NgxSpinnerService,
     public dialog : MatDialog,
     private alertifyService : AlertifyService,
-    private dialogService : DialogService 
+    private dialogService : DialogService
     ) {
         const img = _renderer.createElement("img");
         img.setAttribute("src", "../../../../../assets/delete_icon.png");
@@ -33,52 +33,51 @@ export class DeleteDirective {
         _renderer.appendChild(element.nativeElement, img)
       }
 
-      @Input() id : string;
-      @Input() controller : string;
-      @Output() callback : EventEmitter<any> = new EventEmitter();
+  @Input() id: string;
+  @Input() controller: string;
+  @Output() callback: EventEmitter<any> = new EventEmitter();
 
-      @HostListener("click") //host listener:ilgili directifin kullanıldığı anda devreye girmesini sağlar. metodu işaretleyerek o metodu gerçekleştirir.
-      async onClick(){
-        this.dialogService.openDialog({
-          componentType : DeleteDialogComponent,
-          data : DeleteState.Yes,
-          afterClosed: async () => {
-            this.spinner.show(SpinnerType.BallFussion)
-            const td : HTMLTableCellElement = this.element.nativeElement;
-            // await this.productService.delete(this.id);
-            this.httpClientService.delete({
-              controller: this.controller
-            },this.id).subscribe(data => {
-              $(td.parentElement).animate({
-                opacity: 0,
-                left: "=50",
-                height: "toogle"
-              }, 700, () => {
-                this.callback.emit();
-                this.alertifyService.message("Ürün başarıyla silinmiştir.", {
-                  dismissOthers:true,
-                  messageType : MessageType.Success,
-                  position: Position.BottomRight
-                });
-              });
-            }, (errorResponse : HttpErrorResponse) => {
-              this.spinner.hide(SpinnerType.BallFussion);
-              this.alertifyService.message("Ürün silinemedi. Hata oluştu", {
-                dismissOthers:true,
-                messageType : MessageType.Error,
-                position: Position.BottomRight
-              });
-            });
-          }
+  @HostListener("click")
+  async onclick() {
+    this.dialogService.openDialog({
+      componentType: DeleteDialogComponent,
+      data: DeleteState.Yes,
+      afterClosed: async () => {
+        this.spinner.show(SpinnerType.BallAtom);
+        const td: HTMLTableCellElement = this.element.nativeElement;
+        this.httpClientService.delete({
+          controller: this.controller
+        }, this.id).subscribe(data => {
+          $(td.parentElement).animate({
+            opacity: 0,
+            left: "+=50",
+            height: "toogle"
+          }, 700, () => {
+            this.callback.emit();
+            this.alertifyService.message(`${this.controller == 'roles' ? 'Rol' : 'Ürün'} başarıyla silinmiştir.`, {
+              dismissOthers: true,
+              messageType: MessageType.Success,
+              position: Position.TopRight
+            })
+          });
+        }, (errorResponse: HttpErrorResponse) => {
+          this.spinner.hide(SpinnerType.BallAtom);
+          this.alertifyService.message(`${this.controller == 'roles' ? 'Rol' : 'Ürün'} silinirken beklenmedik bir hatayla karşılaşıldı. Daha sonra tekrar deneyiniz..`, {
+            dismissOthers: true,
+            messageType: MessageType.Error,
+            position: Position.TopRight
+          });
         });
       }
+    });
+  }
 
       // openDialog(afterClosed: any): void {
       //   const dialogRef = this.dialog.open(DeleteDialogComponent, {
       //     width: '250px',
       //     data: DeleteState.Yes,
       //   });
-    
+
       //   dialogRef.afterClosed().subscribe(result => {
       //     if(result == DeleteState.Yes) {-
       //       afterClosed();
